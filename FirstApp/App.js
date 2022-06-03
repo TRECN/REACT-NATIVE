eefc0bcfaff83fa39afbf893fba02d2855e0d92c
TRECN <rishabh1103@outlook.com>
2022-06-03 21:30:50 +0530

    commit

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -1,17 +1,39 @@
 import React from "react";
-import {Text,View} from 'react-native'
+import {Text,View,Image,ScrollView,TextInput} from 'react-native'
 
 const App=()=>{
   return(
-    <View style={{
-      flex:1,
-      justifyContent:"center",
-      alignItems:"center"
-      }}>
+    // <View style={{
+    //   flex:1,
+    //   justifyContent:"center",
+    //   alignItems:"center"
+    //   }}>
+    //   <Text>
+    //     Hello Welcomme to the app
+    //   </Text>
+    // </View>
+
+    <ScrollView>
       <Text>
-        Hello Welcomme to the app
+        Some text
       </Text>
-    </View>
+      <View>
+        <Text>Some more Text</Text>
+        <Image 
+            source={{uri:'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width:200,height:200}}
+        />
+
+      </View>
+      <TextInput
+        style={{
+          height:40,
+          borderColor:'gray',
+          borderWidth:1
+        }}
+        />
+    </ScrollView>
   )
 }
 
