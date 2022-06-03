75b34967b2f1279bda636d9b376df231614047ce
TRECN <rishabh1103@outlook.com>
2022-06-03 21:37:03 +0530

    commit

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -1,40 +1,51 @@
+// import React from "react";
+// import {Text,View,Image,ScrollView,TextInput} from 'react-native'
+
+// const App=()=>{
+//   return(
+//     // <View style={{
+//     //   flex:1,
+//     //   justifyContent:"center",
+//     //   alignItems:"center"
+//     //   }}>
+//     //   <Text>
+//     //     Hello Welcomme to the app
+//     //   </Text>
+//     // </View>
+
+//     <ScrollView>
+//       <Text>
+//         Some text
+//       </Text>
+//       <View>
+//         <Text>Some more Text</Text>
+//         <Image 
+//             source={{uri:'https://reactnative.dev/docs/assets/p_cat2.png',
+//           }}
+//           style={{width:200,height:200}}
+//         />
+
+//       </View>
+//       <TextInput
+//         style={{
+//           height:40,
+//           borderColor:'gray',
+//           borderWidth:1
+//         }}
+//         />
+//     </ScrollView>
+//   )
+// }
+
+// export default App
+
 import React from "react";
-import {Text,View,Image,ScrollView,TextInput} from 'react-native'
+import { View } from "react-native";
 
-const App=()=>{
-  return(
-    // <View style={{
-    //   flex:1,
-    //   justifyContent:"center",
-    //   alignItems:"center"
-    //   }}>
-    //   <Text>
-    //     Hello Welcomme to the app
-    //   </Text>
-    // </View>
+const App =()=>{
+  return (
+    <View>
 
-    <ScrollView>
-      <Text>
-        Some text
-      </Text>
-      <View>
-        <Text>Some more Text</Text>
-        <Image 
-            source={{uri:'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{width:200,height:200}}
-        />
-
-      </View>
-      <TextInput
-        style={{
-          height:40,
-          borderColor:'gray',
-          borderWidth:1
-        }}
-        />
-    </ScrollView>
-  )
+    </View>
+  );
 }
-
-export default App
