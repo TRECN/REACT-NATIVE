a6ac7cec5a6bcd027d788abe8a62b61c48f0289e
TRECN <rishabh1103@outlook.com>
2022-06-06 13:49:35 +0530

    commit

--- FirstApp/components/Section_List.js
+++ FirstApp/components/Section_List.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { StyleSheet } from "react-native";
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop:20
+    },
+    sectionHeader:{
+        padding:5,
+        fontSize:10,
+        fontWeight:'bold',
+        backgroundColor:'green'
+    },
+    item:{
+        padding:'10',
+        fontSize:10,
+        height:44,
+    }
+})
 
 const Section_List=()=>{
-    return(<></>);
+    return();
 }
 
 export default Section_List
