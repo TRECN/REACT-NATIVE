834de3dec1f9f17045ee10cbe346a4a41041cde6
TRECN <rishabh1103@outlook.com>
2022-06-07 17:10:07 +0530

    fetch algo setup using react's movie api

--- FirstApp/components/ApiFetch.js
+++ FirstApp/components/ApiFetch.js
@@ -1,2 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+const ApiFetch=()=>{
+    const [isLoading,setLoading]=useState(true);
+    const [data,setData]=useState([]);
+
+    const getMovie=async()=>{
+        try{
+            const response = await fetch('https://reactnative.dev/movies.json');
+            const json = await response.json();
+            setData(json.movies);
+        }catch(error){
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        getMovie();
+    },[])
+
+    return (<></>);
+}
