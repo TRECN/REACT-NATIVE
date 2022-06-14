5dd6607343d27b0396e5756f28d23df0ef4f63fd
TRECN <rishabh1103@outlook.com>
2022-06-14 22:11:08 +0530

    commit

--- NewsApp/App.js
+++ NewsApp/App.js
@@ -1,6 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+
+const Tab=createBottomTabNavigator();
+
 const App = () => {
   return (
     <View>
