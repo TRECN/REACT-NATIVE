5bc0382bf2f2381b1630f4d5e09e2328d916ada4
TRECN <rishabh1103@outlook.com>
2022-06-14 22:27:30 +0530

    commit

--- NewsApp/screens/All.js
+++ NewsApp/screens/All.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const All = () => {
   return (
     <View>
       <Text>All</Text>
     </View>
-  )
-}
+  );
+};
 
-export default All
+export default All;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
