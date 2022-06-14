a44db0c2f7d6256fd82c959d70bba5cfafd341d5
TRECN <rishabh1103@outlook.com>
2022-06-14 22:28:01 +0530

    commit

--- NewsApp/screens/Sports.js
+++ NewsApp/screens/Sports.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const Sports = () => {
   return (
     <View>
       <Text>Sports</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Sports
+export default Sports;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
