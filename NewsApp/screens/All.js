c2d5a027ec2e6b170181af85c39491e8efa9516e
TRECN <rishabh1103@outlook.com>
2022-06-16 20:41:27 +0530

    commit

---
+++ NewsApp/screens/All.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const All = () => {
+  return (
+    <View>
+      <Text style={{color:'red'}}>Rishabh</Text>
+    </View>
+  );
+};
+
+export default All;
+
+const styles = StyleSheet.create({});
