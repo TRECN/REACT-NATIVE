ac38b0a56cf48d2357f5846f90697d19c18e65ae
TRECN <rishabh1103@outlook.com>
2022-06-17 20:13:32 +0530

    commit

---
+++ NewsApp/App.js
@@ -0,0 +1,37 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import { Icon } from 'react-native-elements';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import All from './screens/All';
+import Business from './screens/Business';
+import Health from './screens/Health';
+import Sports from './screens/Sports';
+import Tech from './screens/Tech';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="All" component={All}
+          options={{
+            tabBarIcon:(props)=>(
+              <Icon type='feather' name='home' color={props.color}/>
+            ),
+          }}
+        />
+        <Tab.Screen name="Business" component={Business} />
+        <Tab.Screen name="Health" component={Health} />
+        <Tab.Screen name="Sports" component={Sports} />
+        <Tab.Screen name="Tech" component={Tech} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
