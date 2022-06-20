e01300142b8d20f87b30f3b13cbe73243abc693e
TRECN <rishabh1103@outlook.com>
2022-06-20 15:36:15 +0530

    commit

--- NewsApp/components/InshortsTab.js
+++ NewsApp/components/InshortsTab.js
@@ -1,5 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View ,useWindowDimensions,} from 'react-native'
+import React,{useState} from 'react'
+
+import { SceneMap, TabView } from 'react-native-tab-view';
 
 const FirstRoute=()=>(
     <View style={{flex:1, backgroundColor:'#ff4081'}} />
@@ -9,12 +11,31 @@ const secondRoute=()=>(
     <View style={{flex:1, backgroundColor:'#673ab7'}} />
 )
 
+const renderScene = SceneMap({
+    first:FirstRoute,
+    second:secondRoute,
+});
 
 const InshortsTab = () => {
+  const layout = useWindowDimensions();
+
+  const [index,setIndex] = useState(0);
+  const [routes]=useState([
+      {key:'first',title:'First'},
+      {key:'second',title:'Second'},
+  ]);
+  
   return (
-    <View>
-      <Text>InshortsTab</Text>
-    </View>
+    <TabView
+        navigationState={{index,routes}}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{width:layout.width}}
+    />
+
+    // <View>
+    //   <Text>InshortsTab</Text>
+    // </View>
   )
 }
 
