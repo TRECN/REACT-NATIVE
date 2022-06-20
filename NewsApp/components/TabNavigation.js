24ba48e5fca23e5ace4ddbf4a0af24e9568ddab9
TRECN <rishabh1103@outlook.com>
2022-06-20 15:54:29 +0530

    commit

--- NewsApp/components/TabNavigation.js
+++ NewsApp/components/TabNavigation.js
@@ -3,12 +3,15 @@ import React from 'react'
 
 const TabNavigation = ({index,setIndex}) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={{...styles.container,backgroundColor:'#282c35'}}>
     </View>
   )
 }
 
 export default TabNavigation
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        
+    }
+})
