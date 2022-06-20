15bd086880eecef895390de961527f3e9883df0a
TRECN <rishabh1103@outlook.com>
2022-06-20 15:20:01 +0530

    commit

---
+++ NewsApp/components/InshortsTab.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InshortsTab = () => {
+  return (
+    <View>
+      <Text>InshortsTab</Text>
+    </View>
+  )
+}
+
+export default InshortsTab
+
+const styles = StyleSheet.create({})
