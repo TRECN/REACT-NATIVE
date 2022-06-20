04a5f633e9b4aa7d000f07557b77499041791813
TRECN <rishabh1103@outlook.com>
2022-06-20 15:29:08 +0530

    commit

--- NewsApp/components/InshortsTab.js
+++ NewsApp/components/InshortsTab.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+const FirstRoute=()=>(
+    <View style={{flex:1, backgroundColor:'#ff4081'}} />
+)
 
+const secondRoute=()=>(
+    <View style={{flex:1, backgroundColor:'#673ab7'}} />
+)
 
 
 const InshortsTab = () => {
