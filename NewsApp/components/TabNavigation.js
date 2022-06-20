438b8a2f24bd9626fd06a2550f227b6261554fba
TRECN <rishabh1103@outlook.com>
2022-06-20 15:55:45 +0530

    commit

---
+++ NewsApp/components/TabNavigation.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TabNavigation = ({index,setIndex}) => {
+  return (
+    <View style={{...styles.container,backgroundColor:'#282c35'}}>
+    </View>
+  )
+}
+
+export default TabNavigation
+
+const styles = StyleSheet.create({
+    container:{
+        
+    }
+})
