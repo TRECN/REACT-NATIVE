8c73995906823b2731e35cd55a9b03a718226e7f
TRECN <rishabh1103@outlook.com>
2022-06-21 21:16:09 +0530

    commit

--- NewsApp/components/TabNavigation.js
+++ NewsApp/components/TabNavigation.js
@@ -1,15 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity} from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react'
-import { TouchableOpacity } from 'react-native-web'
 
 const TabNavigation = ({index,setIndex}) => {
   return (
     <View style={{...styles.container,backgroundColor:'#282c35'}}>
-      {index==='0'?
+      {index===0?
         <TouchableOpacity style={styles.left}>
           <Text style={{...styles.text, color:'black'}}>
-          <MaterialCommunityIcons name="theme-light-dark" size={24} color="black" />
+            <MaterialCommunityIcons name="theme-light-dark" size={24} color="black" />
           </Text>
         </TouchableOpacity>:<></>  
     }
@@ -35,6 +34,6 @@ const styles = StyleSheet.create({
       justifyContent: 'space-between',
     },
     text:{
-      fontSize:16,
+      fontSize:40,
     }
 })
