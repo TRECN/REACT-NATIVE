6ffc36f43ab2cf8ef8a4616e7aa1c518fe8fc868
TRECN <rishabh1103@outlook.com>
2022-06-21 20:56:04 +0530

    commit

--- NewsApp/components/TabNavigation.js
+++ NewsApp/components/TabNavigation.js
@@ -1,9 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-web'
 
 const TabNavigation = ({index,setIndex}) => {
   return (
     <View style={{...styles.container,backgroundColor:'#282c35'}}>
+      {index==='0'?
+        <TouchableOpacity>
+          <Text>
+            
+          </Text>
+        </TouchableOpacity>  
+    }
     </View>
   )
 }
