5447f1eacde6761ca17df658bc7c093942fc5996
TRECN <rishabh1103@outlook.com>
2022-06-22 23:28:34 +0530

    commit

--- Calculator/components/ButtonComp.js
+++ Calculator/components/ButtonComp.js
@@ -1,3 +1,13 @@
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import React from 'react';
+
+const screen = Dimensions.get('window');
+const ButtonWidth = screen.width / 4;
 
 const styles = StyleSheet.create({
   Btn: {
