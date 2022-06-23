300d0ba97f5a5bc5ac7f778b7a85946eedb96056
TRECN <rishabh1103@outlook.com>
2022-06-23 21:44:40 +0530

    commit

--- NewsApp/components/InshortsTab.js
+++ NewsApp/components/InshortsTab.js
@@ -1,3 +1,16 @@
+import { StyleSheet, Text, View ,useWindowDimensions,} from 'react-native'
+import React,{useState} from 'react'
+
+import { SceneMap, TabView } from 'react-native-tab-view';
+import DiscoverScreen from '../screens/DiscoverScreen';
+import NewsScreen from '../screens/NewsScreen';
+import TabNavigation from './TabNavigation'
+
+
+const renderScene = SceneMap({
+    first:DiscoverScreen,
+    second:NewsScreen,
+});
 
 const InshortsTab = () => {
   const layout = useWindowDimensions();
