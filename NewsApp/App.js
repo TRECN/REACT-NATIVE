b6a6d2cb617aa8cba3d3018c7aeb82001678c56c
TRECN <rishabh1103@outlook.com>
2022-06-24 23:04:25 +0530

    commit

---
+++ NewsApp/App.js
@@ -0,0 +1,18 @@
+
+import { StyleSheet, Text, View ,StatusBar} from 'react-native';
+import InshortTab from './components/InshortsTab';
+
+export default function App() {
+  return (
+    <View style={{...styles.container,backgroundColor:'#282C35'}}>
+      <InshortTab/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop:StatusBar.currentHeight,
+  },
+});
