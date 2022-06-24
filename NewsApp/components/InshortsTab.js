9254acb554975310f9516deacd25823e25337f57
TRECN <rishabh1103@outlook.com>
2022-06-24 23:05:09 +0530

    commit

---
+++ NewsApp/components/InshortsTab.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View ,useWindowDimensions,} from 'react-native'
+import React,{useState} from 'react'
+
+import { SceneMap, TabView } from 'react-native-tab-view';
+import DiscoverScreen from '../screens/DiscoverScreen';
+import NewsScreen from '../screens/NewsScreen';
+import TabNavigation from './TabNavigation'
+
+
+const renderScene = SceneMap({
+    first:DiscoverScreen,
+    second:NewsScreen,
+});
+
+const InshortsTab = () => {
+  const layout = useWindowDimensions();
+
+  const [index,setIndex] = useState(0);
+  const [routes]=useState([
+      {key:'first',title:'Discover'},
+      {key:'second',title:'News'},
+  ]);
+  
+  return (
+    <TabView
+        navigationState={{index,routes}}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{width:layout.width}}
+        renderTabBar={()=><TabNavigation
+            index={index}
+            setIndex={setIndex}
+        />}
+    />
+
+    // <View>
+    //   <Text>InshortsTab</Text>
+    // </View>
+  )
+}
+
+export default InshortsTab
+
+const styles = StyleSheet.create({})
