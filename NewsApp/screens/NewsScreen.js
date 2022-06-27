67666fb0c7d5d734d5241da997b3f1217238e8f4
TRECN <rishabh1103@outlook.com>
2022-06-27 20:50:29 +0530

    commit

--- NewsApp/screens/NewsScreen.js
+++ NewsApp/screens/NewsScreen.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-
+const NewsScreen = () => {
+  return (
+    <View>
+      <Text>NewsScreen</Text>
+    </View>
+  )
+}
 
 export default NewsScreen
 
