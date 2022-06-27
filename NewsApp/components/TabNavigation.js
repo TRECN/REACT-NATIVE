1af2a0e09d4013bbeb70f52a5887088de81c036e
TRECN <rishabh1103@outlook.com>
2022-06-27 20:48:33 +0530

    commit

--- NewsApp/components/TabNavigation.js
+++ NewsApp/components/TabNavigation.js
@@ -37,7 +37,14 @@ const TabNavigation = ({index,setIndex}) => {
 export default TabNavigation
 
 const styles = StyleSheet.create({
-    
+    container:{
+      flexDirection:'row',
+      justifyContent:'space-between',
+      padding:10,  
+      alignItems:'center',
+      borderBottomColor:'black',
+      borderBottomWidth:0.5,
+    },
     left:{ 
       flexDirection:'row',
       alignItems:'center',
