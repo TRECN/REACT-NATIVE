0b799b6c225709c36a2b2ceba28150b5ee27af22
TRECN <rishabh1103@outlook.com>
2022-06-27 20:50:10 +0530

    commit

--- NewsApp/components/InshortsTab.js
+++ NewsApp/components/InshortsTab.js
@@ -7,20 +7,6 @@ import NewsScreen from '../screens/NewsScreen';
 import TabNavigation from './TabNavigation'
 
 
-const renderScene = SceneMap({
-    first:DiscoverScreen,
-    second:NewsScreen,
-});
-
-const InshortsTab = () => {
-  const layout = useWindowDimensions();
-
-  const [index,setIndex] = useState(0);
-  const [routes]=useState([
-      {key:'first',title:'Discover'},
-      {key:'second',title:'News'},
-  ]);
-  
   return (
     <TabView
         navigationState={{index,routes}}
