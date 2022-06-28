27e0493093d41fd6b1e399cf257263b7d3620d41
TRECN <rishabh1103@outlook.com>
2022-06-28 21:41:18 +0530

    commit

--- NewsApp/App.js
+++ NewsApp/App.js
@@ -1,11 +1,20 @@
 
 import { StyleSheet, Text, View ,StatusBar} from 'react-native';
 import InshortTab from './components/InshortsTab';
+import React,{ useState } from 'react'
 
 export default function App() {
+  const [mode,setMode]=useState(true);
+  const [col,setCol]=useState('');
+
+  if(mode)
+      setCol('#282C35')
+    else
+      setCol('white')
+
   return (
-    <View style={{...styles.container,backgroundColor:'#282C35'}}>
-      <InshortTab/>
+    <View style={{...styles.container,backgroundColor:col}}>
+      <InshortTab />
     </View>
   );
 }
