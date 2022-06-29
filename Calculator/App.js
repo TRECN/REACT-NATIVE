817ff8b49d4e2f2b14717848016fd680b2791820
TRECN <rishabh1103@outlook.com>
2022-06-29 16:41:09 +0530

    commit

--- Calculator/App.js
+++ Calculator/App.js
@@ -39,4 +39,149 @@ const App = () => {
       </View>
       </ScrollView>
       
-      
+      <Row>
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='AC'
+          theme='secondary'
+        />
+        <Btn 
+        calc={calc}
+        setCalc={setCalc}
+        result={result}
+        setResult={setResult}
+          title='DEL'
+          theme='secondary'
+        />
+        {/* % */}
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='%'
+          theme='secondary'
+        />
+        {/* / */}
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='/'
+          theme='secondary'
+        />
+      </Row>
+      <Row>
+      {createDigits(1,3)}
+      {/* * */}
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='*'
+          theme='secondary'
+        />
+      </Row>
+      <Row>
+        {createDigits(4,6)}
+        {/* - */}
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='-'
+          theme='secondary'
+          
+        />
+      </Row>
+      <Row>
+        {createDigits(7,9)}
+        {/* + */}
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='+'
+          theme='secondary'
+        />
+      </Row>
+      <Row>
+        {/* 0 */}
+        <Btn 
+        calc={calc}
+        setCalc={setCalc}
+        result={result}
+        setResult={setResult}
+          title='0'
+        />
+        {/* . */}
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='.' 
+        />
+        {/* = */}
+        <Btn 
+          calc={calc}
+          setCalc={setCalc}
+          result={result}
+          setResult={setResult}
+          title='='
+          size='double'
+          theme='equal'
+
+        />
+      </Row>
+    </View>
+    
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+    calc:{
+      color:'#fff',
+      fontSize:30,
+      textAlign:'right',
+      marginRight:20,
+      marginTop:40
+    },
+    result:{
+      color:'#808080',
+      fontSize:20,
+      textAlign:'right',
+      marginRight:20,
+      marginBottom:60,
+      marginTop:40
+    },
+    container:{
+      flex:1,
+      flexDirection:'column',
+
+      backgroundColor:'#060606'
+    },
+    operator:{
+      flex:1,
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center'
+    },  
+    digits:{
+      flex:1,
+      flexDirection:'row',
+      justifyContent:'center'
+
+    },
+
+
+})
