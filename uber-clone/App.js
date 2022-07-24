1b34d878b326621686de0310d315aec3f00b2657
TRECN <rishabh1103@outlook.com>
2022-07-24 19:23:51 +0530

    commit

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,21 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{fontSize:40}}>Uber</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    marginTop:50,
-    marginLeft:50,
-  
-  },
-});
