65488146c4051e9a586dc8b46c85159718cf4607
TRECN <rishabh1103@outlook.com>
2022-07-25 18:12:34 +0530

    commit

--- uber-clone/screens/Home.js
+++ uber-clone/screens/Home.js
@@ -6,7 +6,7 @@ import { Image } from 'react-native';
 const Home = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-    <View style={{padding:20}}>
+    <View style={tw`p-5`}>
      <Image 
          style={{width:100,height:100,resizeMode:'contain',}}
          source={{
