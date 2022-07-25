7133c614fa2c3b55d553ad26f923cf09a98f6b65
TRECN <rishabh1103@outlook.com>
2022-07-25 17:27:34 +0530

    commit

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
     <View style={styles.container}>
       <Text style={{fontSize:40}}>Uber</Text>
       <StatusBar style="auto" />
