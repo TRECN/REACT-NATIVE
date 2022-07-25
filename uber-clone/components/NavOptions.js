d470fc669019b0e7f25cb6d61d34592db7c92494
TRECN <rishabh1103@outlook.com>
2022-07-25 18:34:15 +0530

    commit

--- uber-clone/components/NavOptions.js
+++ uber-clone/components/NavOptions.js
@@ -1,4 +1,4 @@
-import {  Text, View ,FlatList,TouchableOpacity } from 'react-native'
+import {  Text, View ,FlatList,TouchableOpacity,Image ,StyleSheet} from 'react-native'
 import React from 'react'
 
 const data=[
@@ -22,10 +22,18 @@ const NavOptions = () => {
   return (
     <FlatList 
         data={data}
+        keyExtractor={(item)=>item.id}
         horizontal
         renderItem={({item})=>(
             <TouchableOpacity>
-                <Text>{item.title}</Text>
+                <View>
+                    <Image
+                    style={styles.img}
+                        source={{
+                            uri:item.image,
+                        }}
+                    />
+                </View>
             </TouchableOpacity>
         )}
     />
@@ -34,3 +42,10 @@ const NavOptions = () => {
 }
 
 export default NavOptions
+const styles = StyleSheet.create({
+    img:{
+        width:120,
+        height:120,
+        resizeMode:'contain'
+    }
+})
