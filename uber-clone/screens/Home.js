04a120ce97f0ea65610047db987ce46ada26041b
TRECN <rishabh1103@outlook.com>
2022-07-25 18:13:19 +0530

    commit

--- uber-clone/screens/Home.js
+++ uber-clone/screens/Home.js
@@ -8,7 +8,11 @@ const Home = () => {
     <SafeAreaView style={tw`bg-white h-full`}>
     <View style={tw`p-5`}>
      <Image 
-         style={{width:100,height:100,resizeMode:'contain',}}
+         style={{
+             width:100,
+             height:100,
+             resizeMode:'contain'
+            }}
          source={{
              uri:'https://links.papareact.com/gzs',
          }}
