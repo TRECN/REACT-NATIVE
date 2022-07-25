958ad11a5fc456017f490a0b40bb16866337de24
TRECN <rishabh1103@outlook.com>
2022-07-25 18:23:32 +0530

    commit

--- uber-clone/components/NavOptions.js
+++ uber-clone/components/NavOptions.js
@@ -1,11 +1,34 @@
-import {  Text, View } from 'react-native'
+import {  Text, View ,FlatList,TouchableOpacity } from 'react-native'
 import React from 'react'
+
+const data=[
+    {
+        id:"1",
+        title:"Get a ride",
+        image:"https://links.papareact.com/3pn",
+        screen:'MapScreen'
+
+    },
+    {
+        id:"2",
+        title:"Order Food",
+        image:"https://links.papareact.com/28w",
+        screem:'EatsScreen'
+
+    }
+]
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <FlatList 
+        data={data}
+        horizontal
+        renderItem={({item})=>{
+            <TouchableOpacity>
+                <Text>{item.title}</Text>
+            </TouchableOpacity>
+        }}
+    />
   )
 }
 
