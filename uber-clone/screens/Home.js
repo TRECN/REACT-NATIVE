0feaf178e734c1461ebcfbd20d353c718774e0b5
TRECN <rishabh1103@outlook.com>
2022-07-25 18:11:47 +0530

    commit

--- uber-clone/screens/Home.js
+++ uber-clone/screens/Home.js
@@ -1,12 +1,21 @@
 import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import { Image } from 'react-native';
 
 const Home = () => {
   return (
-    <SafeAreaView style={tw`bg-red-100 h-full`}>
-        <Text style={[tw`p-10 text-red-500`]}>UBER</Text>
-    </SafeAreaView>
+    <SafeAreaView style={tw`bg-white h-full`}>
+    <View style={{padding:20}}>
+     <Image 
+         style={{width:100,height:100,resizeMode:'contain',}}
+         source={{
+             uri:'https://links.papareact.com/gzs',
+         }}
+     />
+    </View>
+     
+ </SafeAreaView>
   )
 }
 
