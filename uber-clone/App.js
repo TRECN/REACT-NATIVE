6739c23551b8c6250ce01081993b6e37088de180
TRECN <rishabh1103@outlook.com>
2022-07-25 17:17:04 +0530

    commit

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:40}}>Uber</Text>
+      <Text style={{fontSize:40}}>Ubers</Text>
       <StatusBar style="auto" />
     </View>
   );
