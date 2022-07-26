41ae1e515f87b17a0c80d1a84f23aa18c9712a97
TRECN <rishabh1103@outlook.com>
2022-07-26 16:17:16 +0530

    commit

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import Home from './screens/Home';
-import { store } from './store';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import Home from "./screens/Home";
+import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-        <NavigationContainer>
-          <SafeAreaProvider>
-              <Home/> 
-            </SafeAreaProvider> 
-        </NavigationContainer>
-
-      
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
@@ -22,9 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    marginTop:50,
-    marginLeft:50,
-  
+    backgroundColor: "white",
+    marginTop: 50,
+    marginLeft: 50,
   },
 });
