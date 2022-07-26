7e3d6561810fb3efc3498154aa05b68b9892aabc
TRECN <rishabh1103@outlook.com>
2022-07-26 17:12:52 +0530

    commit

--- uber-clone/babel.config.js
+++ uber-clone/babel.config.js
@@ -1,16 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo',"module:metro-react-native-babel-preset",
+    presets: ['babel-preset-expo',
     ],
     plugins: [
       ["module:react-native-dotenv", {
-        "envName": "APP_ENV",
         "moduleName": "@env",
         "path": ".env",
-        "safe": false,
-        "allowUndefined": true,
-        "verbose": false
+        
       }]
     ]
   };
