993314496c6ec7a33c837fa71ab9f5bc2c08fa60
TRECN <rishabh1103@outlook.com>
2022-07-28 18:33:11 +0530

    commit

--- WeatherApp/screens/Home.js
+++ WeatherApp/screens/Home.js
@@ -2,51 +2,18 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
-import GetLocation from 'react-native-get-location';
-import { useState } from 'react';
-import { ActivityIndicator } from 'react-native';
+import {Location,Permission} from 'expo'
+
 
 
 const Home = () => {
 
-    const [location,setLocation]=useState('');
-    const [loading,setLoading]=useState(true)
-    GetLocation.getCurrentPosition({
-        enableHighAccuracy: true,
-        timeout: 15000,
-    })
-    .then(location => {
-        setLocation(location)
-        setLoading(false)
-    })
-    .catch(ex => {
-        const {code, message} = ex;
-        console.warn(code, message);
-        if (code === 'CANCELLED') {
-          Alert.alert('Location cancelled by user or by another request');
-        }
-        if (code === 'UNAVAILABLE') {
-          Alert.alert('Location service is disabled or unavailable');
-        }
-        if (code === 'TIMEOUT') {
-          Alert.alert('Location request timed out');
-        }
-        if (code === 'UNAUTHORIZED') {
-          Alert.alert('Authorization denied');
-        }
-        this.setState({
-          location: null,
-          loading: false,
-        });
-      });
+    
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
         <View style={tw`p-5`}>
-        {loading ? <ActivityIndicator/>: null}
-        {location ? (
-          <Text style={styles.location}>{JSON.stringify(location, 0, 2)}</Text>
-        ) : null}
+        
         </View>
     </SafeAreaView>
   )
