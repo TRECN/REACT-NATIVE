799ecf03a108589cff4b0a77b0577d19fa4aa7f0
TRECN <rishabh1103@outlook.com>
2022-07-29 16:38:33 +0530

    commit

--- WeatherApp/components/currLocation.js
+++ WeatherApp/components/currLocation.js
@@ -1,7 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useState,useEffect } from 'react'
+import * as Location from "expo-location";
 
 const currLocation = () => {
+
+    const [location,setLocation] = useState();
+
+    const getLocation = async()=>{
+        try{
+            const {status} = await Location.requestPermissionsAsync();
+            if(status) {
+                const {
+                    coords:{long,lat},
+                }=await Location.getCurrentPositionAsync();
+                setLocation({long,lat});
+            }
+        }catch(er){
+            console.log(er);
+        }
+    }
+    useEffect
+
   return (
     <View>
       <Text>currLocation</Text>
@@ -10,5 +30,3 @@ const currLocation = () => {
 }
 
 export default currLocation
-
-const styles = StyleSheet.create({})
