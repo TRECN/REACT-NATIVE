b5055e30e9b975aba3c2821fc820726d610c2842
TRECN <rishabh1103@outlook.com>
2022-07-29 16:30:18 +0530

    commit

---
+++ WeatherApp/components/currLocation.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const currLocation = () => {
+  return (
+    <View>
+      <Text>currLocation</Text>
+    </View>
+  )
+}
+
+export default currLocation
+
+const styles = StyleSheet.create({})
