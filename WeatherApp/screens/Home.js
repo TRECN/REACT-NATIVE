90e62055cb4b386f6d05816cecc9273c6d97739d
TRECN <rishabh1103@outlook.com>
2022-07-29 17:05:23 +0530

    commit

--- WeatherApp/screens/Home.js
+++ WeatherApp/screens/Home.js
@@ -7,30 +7,32 @@ import * as Location from "expo-location";
 
 
 const Home = () => {
-  const [location,setLocation] = useState();
+  const [long,setLong]=useState();
+  const [lat,setLat]=useState();
 
-    const getLocation = async()=>{
-        try{
-            const {status} = await Location.requestForegroundPermissionsAsync();
-            if(status) {
-                const {
-                    coords:{long,lat},
-                }=await Location.getCurrentPositionAsync();
-                setLocation({long,lat});
-            }
-        }catch(er){
-            console.log(er);
-        }
+  const getLocation = async () => {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) return;
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      setLat(latitude);
+      setLong(longitude);
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(()=>{
-       getLocation(); 
-    },[])
-    
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
 
   return (
-    <SafeAreaView style={tw`bg-black h-full`}>
-        <View>
-          <Text style={tw`text-lg `}>{JSON.stringify(location)}</Text>
+    <SafeAreaView style={tw`bg-white h-full `}>
+        <View >
+          <Text style={tw`text-lg p-10`}>{long}</Text>
+          <Text style={tw`text-lg p-10`}>{lat}</Text>
         </View>
     </SafeAreaView>
   )
