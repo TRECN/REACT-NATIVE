e611499c53cfae659d5750a747e872ab116a7ca0
TRECN <rishabh1103@outlook.com>
2022-07-29 16:50:24 +0530

    commit

--- WeatherApp/screens/Home.js
+++ WeatherApp/screens/Home.js
@@ -2,18 +2,36 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
-import { currLocation } from "../components/currLocation";
+import { useState,useEffect } from 'react'
+import * as Location from "expo-location";
 
 
 const Home = () => {
+  const [location,setLocation] = useState();
 
+    const getLocation = async()=>{
+        try{
+            const {status} = await Location.requestForegroundPermissionsAsync();
+            if(status) {
+                const {
+                    coords:{long,lat},
+                }=await Location.getCurrentPositionAsync();
+                setLocation({long,lat});
+            }
+        }catch(er){
+            console.log(er);
+        }
+    }
+    useEffect(()=>{
+       getLocation(); 
+    },[])
     
 
   return (
-    <SafeAreaView style={tw`bg-white h-full`}>
-        
-          <currLocation/>
-        
+    <SafeAreaView style={tw`bg-black h-full`}>
+        <View>
+          <Text style={tw`text-lg `}>{JSON.stringify(location)}</Text>
+        </View>
     </SafeAreaView>
   )
 }
